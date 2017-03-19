3933697d71c2e23d974feef0b3c0e85ca20c6839
Ubuntu <ubuntu@ip-172-31-20-175.us-west-2.compute.internal>
2017-03-19 14:55:27 +0000

    working container on ec2

--- web/swagger_server/__main__.py
+++ web/swagger_server/__main__.py
@@ -9,4 +9,4 @@ if __name__ == '__main__':
 	app.app.json_encoder = JSONEncoder
 	app.add_api('swagger.yaml', arguments={'title': 'API developed for the pathfinding storage. To start, post to root to obtain a UID and then post away using the UID'})
 	#app.run(host='ec2-52-41-229-1.us-west-2.compute.amazonaws.com', port=8080)
-	app.run(host='0.0.0.0', port=5000)
+	app.run(host='0.0.0.0', port=8080)
